8210b924a81dc02cc7f937b35bcb100e0c066a96
Sudhamshu B N <66492363+sudhamshu091@users.noreply.github.com>
2021-06-16 21:22:46 +0530

    Update mux_2_to_1_4bit.v

--- mux_2_to_1_4bit/mux_2_to_1_4bit.v
+++ mux_2_to_1_4bit/mux_2_to_1_4bit.v
@@ -1,5 +1,5 @@
 module mux_2_to_1_4bit (SW, LEDR);
 	input [9:0]SW;
-	output [1:0]LEDR;
-	assign LEDR[1:0] = SW[9]? (SW[8]? SW[7:6] : SW[5:4]) : (SW[8] ? SW[3:2] : SW[1:0]);
+	output [3:0]LEDR;
+	assign LEDR[3:0] = SW[9]? SW[8:4] : SW[3:0];
 endmodule
